d9f7c376c1f65007339a0407b30e735279e19eff
kcbigring <kcawley@birdbox.com>
2013-04-09 18:34:46 -0600

    remedy session problems - just force another login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,13 @@ class HomeController < ApplicationController
   def index
     @photos = [] # initialize an array to hold photos
     if session[:omniauth]
-      facebook = Facebook.new
-      token = session[:omniauth]['credentials']['token']
-      @photos = facebook.get_photos(token)
+      begin
+        facebook = Facebook.new
+        token = session[:omniauth]['credentials']['token']
+        @photos = facebook.get_photos(token)
+      rescue
+        @photos = []
+      end
     end
   end
 end
